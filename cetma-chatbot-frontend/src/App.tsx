6abf4725f0fe04d0fbb5e4b66cf2e34763adef45
Checcolino1312 <75864171+Checcolino1312@users.noreply.github.com>
2025-07-03 09:25:07 +0200

    update1

---
+++ cetma-chatbot-frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import Chat from './components/Chat';
+
+function App() {
+  return (
+    <div className="h-screen w-screen overflow-hidden bg-white">
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
